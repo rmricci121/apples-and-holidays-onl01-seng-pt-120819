70c7a552e9183931c06a8a9120828828f2fadb6a
Robert M Ricci <rmricci121@gmail.com>
2019-12-20 01:44:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   puts "#{season.capitalize}:"
     supply.map do |holiday, decorations|
       #binding.pry
-      puts "  #{holiday.to_s.split}: #{decorations.join", "}"
+      puts "  #{holiday.to_s.split"_"}: #{decorations.join", "}"
     end
   end
 end
