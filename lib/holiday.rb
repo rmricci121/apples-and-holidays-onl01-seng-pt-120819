9718d46dfea240c2cf5b48bf84e8e8d3a14969e5
Robert M Ricci <rmricci121@gmail.com>
2019-12-20 01:05:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.map do |holiday, supply|
-  puts ""
+  puts " #{holiday}.capitalize"
+  
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
